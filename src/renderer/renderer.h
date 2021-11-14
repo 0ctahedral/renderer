42cabae0db0a5a5eb6e66e56256a9de1174975b4
Morgan Rosenkranz <rosenkranz1999@gmai.com>
2021-11-14 15:13:02 -0500

    added assertf

--- src/renderer/renderer.h
+++ src/renderer/renderer.h
@@ -1,7 +1,13 @@
 #include <GLFW/glfw3.h>
 #include <vulkan/vulkan.h>
+#include <stdio.h>
 #include <stdbool.h>
+#include <assert.h>
 // this will be the renderer api
+//
+
+// assersion
+#define assertf(A, M, ...) if(!(A)) {fprintf(stderr, "%s:%d: " M "\n", __FILE__, __LINE__,##__VA_ARGS__); assert(A); }
 
 /// open a window
 GLFWwindow* open_window();
